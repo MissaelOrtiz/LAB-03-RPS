ce22b41bab3f7d3ace8a6ebceaa74dcd462b0969
Missael Ortiz <ortiz.missael80@gmail.com>
2021-05-05 14:00:28 -0700

    added functions

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // import functions and grab DOM elements
-
+import { compareThrow, getRandomThrow} from './utils.js';
 // initialize state
+let totalPlays = 0;
+let totalWins = 0;
+let oppThrow = Math.ceil(Math.random() * 3);
 
 // set event listeners to update state and DOM
---
+++ utils.js
@@ -0,0 +1,29 @@
+export function getRandomThrow(someNumber) {
+    if (someNumber === 3) {
+        return 'Bolder';
+    }
+    if (someNumber === 2) {
+        return 'Parchment';
+    }
+    if (someNumber === 1) {
+        return 'Shears';
+    }
+}
+
+export function compareThrow(userThrow, computerThrow) {
+    if (userThrow === computerThrow) {
+        return 0;
+    } else if (userThrow === 'Bolder' && computerThrow === 'Parchment') {
+        return -1;
+    } else if (userThrow === 'Bolder' && computerThrow === 'Shears') {
+        return 1;
+    } else if (userThrow === 'Parchment' && computerThrow === 'Bolder') {
+        return 1;
+    } else if (userThrow === 'Parchment' && computerThrow === 'Shears') {
+        return -1;
+    } else if (userThrow === 'Shears' && computerThrow === 'Parchment') {
+        return +1;
+    } else if (userThrow === 'Shears' && computerThrow === 'Bolder') {
+        return +1;
+    }
+}
